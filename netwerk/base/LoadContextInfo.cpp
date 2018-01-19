Floorp-Projects/Floorp

Bug 1329521 - GetLoadContextInfo() should not compare originAttributes and privateBrowsing boolean when docShell is chrome type, r=smaug

netwerk/base/LoadContextInfo.cpp
@@ -5,6 +5,7 @@
 #include "LoadContextInfo.h"
 
 #include "mozilla/dom/ToJSValue.h"
+#include "nsDocShell.h"
 #include "nsIChannel.h"
 #include "nsILoadContext.h"
 #include "nsIWebNavigation.h"
@@ -143,12 +144,17 @@ GetLoadContextInfo(nsILoadContext *aLoadContext, bool aIsAnonymous)
     return new LoadContextInfo(aIsAnonymous, OriginAttributes());
   }
 
-  DebugOnly<bool> pb = aLoadContext->UsePrivateBrowsing();
   OriginAttributes oa;
   aLoadContext->GetOriginAttributes(oa);
   oa.StripAttributes(OriginAttributes::STRIP_ADDON_ID);
 
-  MOZ_ASSERT(pb == (oa.mPrivateBrowsingId > 0));
+#ifdef DEBUG
+  nsCOMPtr<nsIDocShellTreeItem> docShell = do_QueryInterface(aLoadContext);
+  if (!docShell || docShell->ItemType() != nsIDocShellTreeItem::typeChrome) {
+    MOZ_ASSERT(aLoadContext->UsePrivateBrowsing() == (oa.mPrivateBrowsingId > 0));
+  }
+#endif
+
   return new LoadContextInfo(aIsAnonymous, oa);
 }
 

muelli/tor-browser

Backported to ESR52: https://bugs.torproject.org/22462

netwerk/base/LoadContextInfo.cpp
@@ -5,6 +5,7 @@
 #include "LoadContextInfo.h"
 
 #include "mozilla/dom/ToJSValue.h"
+#include "nsDocShell.h"
 #include "nsIChannel.h"
 #include "nsILoadContext.h"
 #include "nsIWebNavigation.h"
@@ -141,13 +142,18 @@ GetLoadContextInfo(nsILoadContext *aLoadContext, bool aIsAnonymous)
                                NeckoOriginAttributes(nsILoadContextInfo::NO_APP_ID, false));
   }
 
-  DebugOnly<bool> pb = aLoadContext->UsePrivateBrowsing();
   DocShellOriginAttributes doa;
   aLoadContext->GetOriginAttributes(doa);
-  MOZ_ASSERT(pb == (doa.mPrivateBrowsingId > 0));
 
   NeckoOriginAttributes noa;
   noa.InheritFromDocShellToNecko(doa);
+
+#ifdef DEBUG
+  nsCOMPtr<nsIDocShellTreeItem> docShell = do_QueryInterface(aLoadContext);
+  if (!docShell || docShell->ItemType() != nsIDocShellTreeItem::typeChrome) {
+    MOZ_ASSERT(aLoadContext->UsePrivateBrowsing() == (doa.mPrivateBrowsingId > 0));
+  }
+#endif
 
   return new LoadContextInfo(aIsAnonymous, noa);
 }