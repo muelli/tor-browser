cd539b2196941c1e0d93a5cd08f28f8df7dde25e
Richard Pospesel <richard@torproject.org>
2018-03-01 14:12:08 -0800

    Bug 25147: Sanitize HTML fragments created for chrome-privileged documents
    Ported over firefox patch c2db4a50dc5c (Bug 1432966)

--- dom/base/Element.cpp
+++ dom/base/Element.cpp
@@ -3564,6 +3564,12 @@ Element::SetInnerHTML(const nsAString& aInnerHTML, ErrorResult& aError)
 }
 
 void
+Element::UnsafeSetInnerHTML(const nsAString& aInnerHTML, ErrorResult& aError)
+{
+  SetInnerHTMLInternal(aInnerHTML, aError, true);
+}
+
+void
 Element::GetOuterHTML(nsAString& aOuterHTML)
 {
   GetMarkup(true, aOuterHTML);
