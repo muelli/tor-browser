Floorp-Projects/Floorp

Bug 1334776 - ResponseHeader Visitor will get original header casing. We need to fix it at some places in devtools.

devtools/client/netmonitor/test/browser_net_filter-01.js
@@ -325,14 +325,15 @@ add_task(function* () {
     const visibleItems = getDisplayedRequests(gStore.getState());
 
     is(items.size, visibility.length,
-      "There should be a specific amount of items in the requests menu.");
+       "There should be a specific amount of items in the requests menu.");
     is(visibleItems.size, visibility.filter(e => e).length,
-      "There should be a specific amount of visible items in the requests menu.");
+       "There should be a specific amount of visible items in the requests menu.");
 
     for (let i = 0; i < visibility.length; i++) {
       let itemId = items.get(i).id;
       let shouldBeVisible = !!visibility[i];
       let isThere = visibleItems.some(r => r.id == itemId);
+
       is(isThere, shouldBeVisible,
         `The item at index ${i} has visibility=${shouldBeVisible}`);
 

muelli/tor-browser

Backported to TBB/ESR52 by Arthur Edelstein <arthuredelstein@gmail.com>
Fixes bug 25938.

devtools/client/netmonitor/test/browser_net_filter-01.js
@@ -181,9 +181,9 @@ add_task(function* () {
       "The details pane should still be visible after filtering.");
 
     is(RequestsMenu.items.length, visibility.length,
-      "There should be a specific amount of items in the requests menu.");
+       "There should be a specific amount of items in the requests menu.");
     is(RequestsMenu.visibleItems.length, visibility.filter(e => e).length,
-      "There should be a specific amount of visbile items in the requests menu.");
+       "There should be a specific amount of visbile items in the requests menu.");
 
     for (let i = 0; i < visibility.length; i++) {
       is(RequestsMenu.getItemAtIndex(i).target.hidden, !visibility[i],